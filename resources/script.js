fbf1fcfaf67db70e827ef7d261342903fe3391ac
Aslam Anver <slam.anver9@gmail.com>
2020-04-07 03:25:33 +0530

    Under development stage 1

---
+++ resources/script.js
@@ -0,0 +1,127 @@
+/* eslint-disable no-unused-vars */
+// @ts-nocheck
+/* eslint-disable no-undef */
+
+// const vscode = undefined
+const vscode = acquireVsCodeApi()
+var packageJsonUri = ''
+var dependencies = []
+var processDependencies = []
+
+const cardHTML = (data) => `
+<div class="dependency-card">
+
+    ${data.name} <span class="version">${data.version} ${data.installed ? '- Installed' : ''}</span>
+
+    <div class="dependency-description">
+        
+        ${data.description}
+        
+        ${ data.repository !== undefined ? '<div class="repo-link"><a href="' + data.repository + '">Repository</a></div>' : ''}
+
+        <div class="other-info">
+            <img src="https://img.shields.io/npm/dm/${data.name}.svg" />
+            <img src="https://img.shields.io/npm/v/${data.name}.svg">
+        </div>
+    </div>
+
+    <div class="manage-btns">
+        <div onclick="managePackage(this, '${data.name}', '${data.installed ? 'remove' : 'add'}')" class="dependency-btn">
+            <i class="material-icons">${data.installed ? 'clear' : 'add'}</i>
+        </div>
+    </div>
+
+</div>`
+
+$(document).ready(function () {
+
+    packageJsonUri = document.getElementById('package-json-uri').value;
+
+    packageJSON()
+})
+
+function packageJSON() {
+
+    $.get(packageJsonUri, (data, status) => {
+
+        dependencies = objectsToArray(data.dependencies).reverse().concat(objectsToArray(data.devDependencies).reverse());
+
+        $('#dependency-list').html('');
+
+        $('#dependency-count').html(`${dependencies.length} dependencies`)
+
+        dependencies.forEach((data) => {
+            $('#dependency-list').append(cardHTML({
+                name: data.name,
+                version: data.version,
+                description: '',
+                repository: undefined,
+                installed: true
+            }))
+        })
+
+        sendVSCode('loaded', 'Document loaded to DOM')
+    })
+}
+
+function objectsToArray(objects) {
+    let arr = []
+    for (var key in objects) {
+        arr.push({ name: key, version: objects[key] })
+    }
+    return arr
+}
+
+function sendVSCode(command, text) {
+
+    if (vscode !== undefined) {
+        vscode.postMessage({ command, text })
+    }
+}
+
+function managePackage(e, name, action) {
+
+    const card = $(e).parent().parent()
+
+    card.find('.manage-btns').hide()
+
+    card.find('.version').text(` will be ${action == 'add' ? 'added' : 'removed'}, please check terminal for more details`)
+
+    sendVSCode(action, name)
+}
+
+function clearSearch() {
+    $('#dependency-input').val('')
+    packageJSON()
+}
+
+function npmFind(e) {
+
+    let query = e.value
+
+    if (query.length >= 3) {
+
+        $('#dependency-search-clear-btn').css('display', 'flex');
+
+        $.get('https://registry.npmjs.com/-/v1/search?text=' + query.toLowerCase(), (data, status) => {
+
+            $('#dependency-list').html('')
+
+            $('#dependency-count').html(`${data.objects.length} dependencies`)
+
+            data.objects.forEach(entry => {
+                $('#dependency-list').append(cardHTML({
+                    name: entry.package.name,
+                    version: entry.package.version,
+                    description: entry.package.description,
+                    repository: entry.package.links.repository,
+                    installed: entry.package.name in dependencies
+                }))
+            })
+        })
+
+    } else {
+        $('#dependency-search-clear-btn').css('display', 'none');
+        packageJSON()
+    }
+}
--- src/extension.js
+++ src/extension.js
@@ -1,21 +1,173 @@
+const path = require('path');
 const vscode = require('vscode');
+
+const terminal = vscode.window.createTerminal(`Node.js Dependencies`);
+
+var configuration = name => vscode.workspace.getConfiguration().get(name);
+
+var mContext = null;
+var mPanel = null;
+var isVisible = false;
 
 function activate(context) {
 
-	console.log('Congratulations, your extension "node-js-dependency-viewer" is now active!');
+	mContext = context;
 
-	let command = vscode.commands.registerCommand('extension.node-js-dependency-viewer', function () {
+	console.log('Congratulations, your extension "node-js-dependency-manager" is now active!');
 
-		vscode.window.createWebviewPanel('vsc-extension', 'Title', vscode.ViewColumn.One, {});
+	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.command = 'extension.node-js-dependency-manager';
+	statusBarItem.text = "$(zap) Node.js Dependencies";
+	statusBarItem.show();
 
+	let command = vscode.commands.registerCommand('extension.node-js-dependency-manager', function () {
+
+		if (isVisible) return;
+
+		mPanel = vscode.window.createWebviewPanel('nodejs-extension', 'Please wait..', vscode.ViewColumn.One, {
+			enableScripts: true,
+			localResourceRoots: [
+				vscode.Uri.file(path.join(context.extensionPath, 'resources')),
+				vscode.Uri.file(vscode.workspace.rootPath)
+			]
+		});
+
+		mPanel.webview.html = addHTML();
+
+		mPanel.webview.onDidReceiveMessage((message) => {
+			switch (message.command) {
+				case 'loaded':
+					mPanel.title = 'Node.js Dependency Manager'
+					return;
+				case 'add':
+					if (configuration('nodejs-dm.showTerminal')) terminal.show();
+					terminal.sendText('npm install -s ' + message.text);
+					return;
+				case 'remove':
+					if (configuration('nodejs-dm.showTerminal')) terminal.show();
+					terminal.sendText('npm uninstall ' + message.text);
+					return;
+			}
+		});
+
+		isVisible = true;
+		mPanel.onDidDispose(() => {
+			isVisible = false;
+		})
 	});
 
+	context.subscriptions.push(statusBarItem);
 	context.subscriptions.push(command);
 }
 
 exports.activate = activate;
 
 function deactivate() { }
+
+
+function addHTML() {
+
+	return `
+		<!doctype html>
+		<html lang="en">
+		
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+			<title>Node.js Dependency Manager</title>
+			
+			<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+			<!--
+			<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+			<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+			<link rel="stylesheet" href="${getFileUri('material.indigo-pink.min.css')}">
+			<script defer src="${getFileUri('material.min.js')}"></script>
+			-->
+
+			<script src="${getFileUri('jquery.min.js')}"></script>
+			<link rel="stylesheet" href="${getFileUri('style.css')}">
+			<script src="${getFileUri('script.js')}"></script>
+		</head>
+		
+		<body>
+
+			<input type="hidden" id="package-json-uri" value="${getHostFileUri('package.json')}">
+
+			<div class="flex-h-center">
+		
+				<div class="body-container">
+		
+					<button onclick="clearSearch()" class="sync-btn ripple">
+						<i class="material-icons">sync</i> SYNC
+					</button>
+		
+					<div class="dependency-search">
+						<input onkeyup="npmFind(this)" id="dependency-input" type="text" class="dependency-input"
+							placeholder="Search keyword..." />
+						<div onclick="clearSearch()" class="dependency-btn dependency-search-clear-btn" id="dependency-search-clear-btn">
+							<i class="material-icons">clear</i>
+						</div>
+					</div>
+		
+					<div class="dependency-count" id="dependency-count">
+						...
+					</div>
+		
+					<div class="dependency-list" id="dependency-list">
+		
+						<div class="dependency-card">
+		
+							Sample <span class="version">^7.1.6</span>
+		
+							<div class="dependency-description">
+		
+								Lorem ipsulum bajali new man from
+		
+								<div class="other-info">
+									<img onerror="this.style.display='none'" src="https://img.shields.io/npm/dm/glob.svg" />
+									<img onerror="this.style.display='none'" src="https://img.shields.io/npm/v/glob.svg">
+								</div>
+							</div>
+		
+							<div class="manage-btns">
+								<div onclick="managePackage(this, 'name', 'remove')" class="dependency-btn">
+									<i class="material-icons">clear</i>
+								</div>
+							</div>
+		
+						</div>
+		
+					</div>
+		
+				</div>
+		
+			</div>
+		
+		</body>
+		
+		</html>`;
+}
+
+function getFileUri(name) {
+
+	const file = vscode.Uri.file(
+		path.join(mContext.extensionPath, 'resources', name)
+	);
+
+	return mPanel.webview.asWebviewUri(file);
+}
+
+function getHostFileUri(name) {
+
+	const file = vscode.Uri.file(
+		path.join(vscode.workspace.rootPath, name)
+	);
+
+	return mPanel.webview.asWebviewUri(file);
+}
 
 module.exports = {
 	activate,
